64582340a4810de9cc36ce4b5bc1fba7af58c277
sslupsky <sslupsky@gmail.com>
2019-10-11 10:34:46 -0600

    remote blocking

--- src/MKRWAN.h
+++ src/MKRWAN.h
@@ -502,12 +502,15 @@ public:
     pollInterval = secs * 1000;
   }
 
-  void poll() {
-    if (millis() - lastPollTime < pollInterval) return;
-    lastPollTime = millis();
+/**
+ * @brief Poll modem for status of a confirmed packet
+ * 
+ * @return int >0 indicates packet sent.  <0 indicates LORA_ERROR.
+ */
+  int poll() {
     // simply trigger a fake write
     uint8_t dummy = 0;
-    modemSend(&dummy, 1, true);
+    return modemSend(&dummy, 1, true);
   }
 
   bool factoryDefault() {
@@ -710,6 +713,20 @@ public:
     return fcd;
   }
 
+/**
+ * @brief Trigger the modem to send received data
+ * 
+ * @return true if we received data from modem
+ * @return false if no response or LORA_ERROR
+ */
+  bool sendRXData() {
+    sendAT(GF("+RECV?"));
+    if (waitResponse(10) != 99) {
+      return false;
+    }
+    return true;
+  }
+
 
 private:
 
@@ -783,11 +800,6 @@ private:
     uint8_t rc = waitResponse( GFP(LORA_OK), GFP(LORA_ERROR), GFP(LORA_ERROR_PARAM), GFP(LORA_ERROR_BUSY), GFP(LORA_ERROR_OVERFLOW), GFP(LORA_ERROR_NO_NETWORK), GFP(LORA_ERROR_RX), GFP(LORA_ERROR_UNKNOWN) );
     if (rc != 1) {
       return rc;
-    }
-    if (confirmed) {
-        if (waitResponse(10000L, "+ACK") != 1) {
-            return -1;
-        }
     }
     return len;
   }
@@ -896,6 +908,8 @@ private:
                 i++;
             }
           }
+          index = 99;
+          goto finish;
         }
       }
     } while (millis() - startMillis < timeout);
